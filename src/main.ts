bef391b7f578b6ca7b3cb2f73050d18cd47bc929
DmitryZuiko <zuiko.z@mail.ru>
2025-07-07 20:24:44 +0300

    feat: get port from env

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { EnvVariables } from './common/enums/envVariables.enum';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -7,7 +9,8 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.enableCors();
 
-  const port = 3000;
+  const configService = app.get(ConfigService);
+  const port = configService.get<string>(EnvVariables.PORT);
 
   await app.listen(port);
 
