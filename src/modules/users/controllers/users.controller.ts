69a0c64f5ac280bca70a7324838865c891989284
DmitryZuiko <zuiko.z@mail.ru>
2025-07-17 18:18:44 +0300

    fix: updateUserDto

--- src/modules/users/controllers/users.controller.ts
+++ src/modules/users/controllers/users.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Param, Patch, Post } from '@nestjs/common';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { UsersService } from '../services/users.service';
+import { UpdateUserDto } from '../dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -12,7 +13,7 @@ export class UsersController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() dto: Partial<CreateUserDto>) {
+  update(@Param('id') id: string, @Body() dto: UpdateUserDto) {
     return this.usersService.update(id, dto);
   }
 }
--- src/modules/users/services/users.service.ts
+++ src/modules/users/services/users.service.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 import { User } from '../entities/user.entity';
 import logger from 'src/common/utils/logger';
 import { CreateUserDto } from '../dto/create-user.dto';
+import { UpdateUserDto } from '../dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -53,7 +54,7 @@ export class UsersService {
     }
   }
 
-  async update(id: string, dto: Partial<CreateUserDto>): Promise<User> {
+  async update(id: string, dto: UpdateUserDto): Promise<User> {
     try {
       const user = await this.userRepository.preload({ id, ...dto });
 
